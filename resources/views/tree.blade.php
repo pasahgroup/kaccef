fc3ff6ad4530567f5ded7f68fde7ff32a34fce8e
pasahgroup <info@pasah.net>
2023-12-04 11:07:54 +0300

    04 Dec 2023

--- resources/views/tree.blade.php
+++ resources/views/tree.blade.php
@@ -1,94 +1,109 @@
 @extends('layouts.apps')
 @section('content')
-<style type="text/css">
-  img {
-  max-width: 100%;
-  height: auto;
-}
-</style>
-
-   <div class="more-info about-info">
+    <div class="main">
       <div class="container">
-
-         <div class="request-form">
+        <ul class="breadcrumb">
+            <li><a href="index.html">Home</a></li>
+            <!-- <li><a href="javascript:;">Blog</a></li> -->
+            <li class="active">Tree Planting</li>
+        </ul>
+        <!-- BEGIN SIDEBAR & CONTENT -->
+        <div class="row margin-bottom-40">
+          <!-- BEGIN CONTENT -->
+          <div class="col-md-12 col-sm-12">
+              <div class="request-form">
       <div class="container">
         <div class="row">
           <div class="col-md-12">
-            <span>Tree Planting</span>
+            <h3>Tree Planting</h3>
           </div>
         </div>
       </div>
     </div>
     <hr>
-
-        <div class="row">
-          <div class="col-md-12">
-            <div class="more-info-content">
+            <div class="content-page">
               <div class="row">
-                <div class="col-md-6 align-self-center">
-                  <div class="right-content">
-                    <h5>Tree <em>Planting</em></h5>
-                    <p>Fusce nec ultrices lectus. Duis nec scelerisque risus. Ut id tempor turpis, ac dignissim ipsum. Nulla ullamcorper, ipsum vel condimentum congue, mi odio vehicula tellus, sit amet malesuada justo sem. 
-                    <br><br>Pellentesque in sagittis lacus, vel auctor sem. Quisque eu quam eleifend, ullamcorper dui nec, luctus quam.</p>
+                <!-- BEGIN LEFT SIDEBAR -->            
+                <div class="col-md-12 col-sm-12 blog-posts">
+                  <div class="row">
+                    <div class="col-md-4 col-sm-4">
+                      <!-- BEGIN CAROUSEL -->            
+                      <div class="front-carousel">
+                        <div class="carousel slide" id="myCarousel">
+                          <!-- Carousel items -->
+                          <div class="carousel-inner">
+                            <div class="item">
+                              <img alt="" src="img/photos/tree-watering.jpg">
+                            </div>
+                            <div class="item">
+                              <img alt="" src="img/photos/tree-watering2.jpg">
+                            </div>
+                            <div class="item active">
+                              <img alt="" src="img/photos/tree-watering4.jpg">
+                            </div>
+                          </div>
+                          <!-- Carousel nav -->
+                          <a data-slide="prev" href="#myCarousel" class="carousel-control left">
+                            <i class="fa fa-angle-left"></i>
+                          </a>
+                          <a data-slide="next" href="#myCarousel" class="carousel-control right">
+                            <i class="fa fa-angle-right"></i>
+                          </a>
+                        </div>                
+                      </div>
+                      <!-- END CAROUSEL -->             
+                    </div>
+                    <div class="col-md-8 col-sm-8">
+                      <h2><a href="blog-item.html">Tree planting activities</a></h2>
+                      <ul class="blog-info">
+                        <li><i class="fa fa-calendar"></i> 25/07/2013</li>
+                        <li><i class="fa fa-comments"></i> 17</li>
+                        <li><i class="fa fa-tags"></i> Kasulu,Kigoma,Tanzania</li>
+                      </ul>
+                      <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui sint blanditiis prae sentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non eleifend enim a feugiat. Pellentesque viverra vehicula sem ut volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing condimentum eleifend enim a feugiat.</p>
+                    </div>
                   </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="left-image">
-                    <img src="img/photos/tree-watering.jpg" alt="">
+                  <hr class="blog-post-sep">
+                  <div class="row">
+                    <div class="col-md-4 col-sm-4">
+                      <img class="img-responsive" alt="" src="img/photos/tree-watering2.jpg">
+                    </div>
+                    <div class="col-md-8 col-sm-8">
+                      <h2><a href="blog-item.html">Activities 2</a></h2>
+                      <ul class="blog-info">
+                        <li><i class="fa fa-calendar"></i> 25/07/2013</li>
+                        <li><i class="fa fa-comments"></i> 17</li>
+                       <li><i class="fa fa-tags"></i> Kasulu,Kigoma,Tanzania</li>
+                      </ul>
+                      <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui sint blanditiis prae sentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non eleifend enim a feugiat. Pellentesque viverra vehicula sem ut volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing condimentum eleifend enim a feugiat.</p>
+                     
+                    </div>
                   </div>
+                  <hr class="blog-post-sep">
+                  <div class="row">
+                    <div class="col-md-4 col-sm-4">
+                      <!-- BEGIN VIDEO -->   
+                      <iframe height="205" allowfullscreen="" style="width:100%; border:0" src="http://player.vimeo.com/video/56974716?portrait=0"></iframe>
+                      <!-- END VIDEO -->   
+                    </div>
+                    <div class="col-md-8 col-sm-8">
+                      <h2><a href="blog-item.html">Activities 3</a></h2>
+                      <ul class="blog-info">
+                        <li><i class="fa fa-calendar"></i> 25/07/2013</li>
+                        <li><i class="fa fa-comments"></i> 17</li>
+                        <li><i class="fa fa-tags"></i>Kasulu,Kigoma,Tanzania</li>
+                      </ul>
+                      <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui sint blanditiis prae sentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non eleifend enim a feugiat. Pellentesque viverra vehicula sem ut volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing condimentum eleifend enim a feugiat.</p>                     
+                    </div>
+                  </div>                                           
                 </div>
-              </div>
-<hr>
-                <div class="row">
-                <div class="col-md-6 align-self-center">
-                  <div class="right-content">
-                    <h5>Preparing <em>Small Trees</em></h5>
-                    <p>Fusce nec ultrices lectus. Duis nec scelerisque risus. Ut id tempor turpis, ac dignissim ipsum. Nulla ullamcorper, ipsum vel condimentum congue, mi odio vehicula tellus, sit amet malesuada justo sem. 
-                    <br><br>Pellentesque in sagittis lacus, vel auctor sem. Quisque eu quam eleifend, ullamcorper dui nec, luctus quam.</p>
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="left-image">
-                    <img src="img/photos/tree-watering2.jpg" alt="">
-                  </div>
-                </div>
-              </div>
-
-              <hr>
-                <div class="row">
-                <div class="col-md-6 align-self-center">
-                  <div class="right-content">
-                    <h5>Preparing <em>Small Trees</em></h5>
-                    <p>Fusce nec ultrices lectus. Duis nec scelerisque risus. Ut id tempor turpis, ac dignissim ipsum. Nulla ullamcorper, ipsum vel condimentum congue, mi odio vehicula tellus, sit amet malesuada justo sem. 
-                    <br><br>Pellentesque in sagittis lacus, vel auctor sem. Quisque eu quam eleifend, ullamcorper dui nec, luctus quam.</p>
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="left-image">
-                    <img src="img/photos/tree-watering4.jpg" alt="">
-                  </div>
-                </div>
-              </div>
-
-                <hr>
-                <div class="row">
-                <div class="col-md-6 align-self-center">
-                  <div class="right-content">
-                    <h5>Preparing <em>Small Trees</em></h5>
-                    <p>Fusce nec ultrices lectus. Duis nec scelerisque risus. Ut id tempor turpis, ac dignissim ipsum. Nulla ullamcorper, ipsum vel condimentum congue, mi odio vehicula tellus, sit amet malesuada justo sem. 
-                    <br><br>Pellentesque in sagittis lacus, vel auctor sem. Quisque eu quam eleifend, ullamcorper dui nec, luctus quam.</p>
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="left-image">
-                    <img src="img/photos/tree-watering3.jpg" alt="">
-                  </div>
-                </div>
+                <!-- END LEFT SIDEBAR -->           
               </div>
             </div>
           </div>
+          <!-- END CONTENT -->
         </div>
+        <!-- END SIDEBAR & CONTENT -->
       </div>
-    </div> 
-
+    </div>  
 @endsection
